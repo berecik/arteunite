eb74dc4cb9cf36b8f53476cee06a6a6f87dda0b4
MaciejPrzybycien <m.a.przybycien@gmail.com>
2021-02-16 03:09:37 +0000

    jquerry elements
    - Dowiedz się main button conection
    - sticky navigation
    -moblile navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,21 +2,41 @@
 
 
 
-/*Scrall on buttons*/
+
 $(document).ready(function(){
 	
+/*Sticky nav*/
+	$('.js--section-features').waypoint(function(direction){
+		if (direction == "down") {
+			$('nav').addClass('sticky');
+		} else {
+			$('nav').removeClass('sticky');
+		}
+	}, {
+		offset:'60px'
 	
-	$('.js--scroll-to-features').click(function (){
-		$('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
-		
 	});
 	
-/* Mobile nav*/
-
+/*Scroll to buttons*/
+	$('.js--scroll-to-features').click(function(){
+		$('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+	});
+	
+/*Mobile nav*/
 	$('.js--nav-icon').click(function(){
 		var nav = $('.js--main-nav');
-		
+		var icon = $('.js--nav-icon i');
 		nav.slideToggle(200);
 		
+		if (icon.hasName('reorder-four-outline')) {
+			icon.addName('close-outline');
+			icon.removeName('reorder-four-outline');
+		} else {
+			icon.addName('reorder-four-outline');
+			icon.removeName('close-outline');
+		}
 	});
+	
 });
+	
+close-outline
